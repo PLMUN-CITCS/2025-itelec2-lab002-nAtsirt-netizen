82048fe8054a5f6b34ba2dce5202fd5110b06acd
TRISTAN BELDAD <beldadtristandave_bsit@plmun.edu.ph>
2025-02-09 18:11:37 -0800

    Update problem_02.py

--- problem_02.py
+++ problem_02.py
@@ -1,13 +1,21 @@
 # TRISTAN DAVE BELDAD
 # ITELEC2
-# Problem Set 01 - Problem 02
-# Square the Number Program
+# Problem Set 01 - Problem 01
+# Simple Calculator Program
+
 def main():
-    print("Square the Number Program")
+    print("Simple Calculator Program")
 
-    number = int(input("Enter a number: "))
+    num1 = int(input("Enter 1st number: "))
+    num2 = int(input("Enter 2nd number: "))
 
-    print(f"The square of {number} is {(number * number):.2f}")
+    print(f"The sum is {num1 + num2}")
 
-if _name_ == "_main_":
+    print(f"The difference is {num1 - num2}")
+
+    print(f"The product is {num1 * num2}")
+
+    print(f"The quotient is {(num1 / num2):.2f}")
+
+if __name__ == "__main__":
     main()
