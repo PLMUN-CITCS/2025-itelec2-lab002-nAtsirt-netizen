516ecc101b6fbf9c013abeb90b76c21ec9329965
TRISTAN BELDAD <beldadtristandave_bsit@plmun.edu.ph>
2025-02-10 09:57:16 +0800

    Update problem_02.py

--- problem_02.py
+++ problem_02.py
@@ -1,10 +1,14 @@
-# YOUR NAME
+# TRISTAN DAVE BELDAD
 # ITELEC2
 # Problem Set 01 - Problem 02
 # Square the Number Program
 
 def main():
-    pass # replace this line with all of your code
+    print("Square the Number Program")
 
-if __name__ == "__main__":
+    number = int(input("Enter a number: "))
+
+    print(f"The square of {number} is {(number * number):.2f}")
+
+if _name_ == "_main_":
     main()
