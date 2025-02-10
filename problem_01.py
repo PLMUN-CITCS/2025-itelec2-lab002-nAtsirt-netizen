b146d36695d5e0482d76f627944bbf8723e0f1ce
TRISTAN BELDAD <beldadtristandave_bsit@plmun.edu.ph>
2025-02-10 09:56:14 +0800

    Update problem_01.py

--- problem_01.py
+++ problem_01.py
@@ -1,10 +1,21 @@
-# YOUR NAME
+# TRISTAN DAVE BELDAD
 # ITELEC2
 # Problem Set 01 - Problem 01
 # Simple Calculator Program
 
 def main():
-    pass # replace this line with all of your code
+    print("Simple Calculator Program")
 
-if __name__ == "__main__":
+    num1 = int(input("Enter 1st number: "))
+    num2 = int(input("Enter 2nd number: "))
+
+    print(f"The sum is {num1 + num2}")
+
+    print(f"The difference is {num1 - num2}")
+
+    print(f"The product is {num1 * num2}")
+
+    print(f"The quotient is {(num1 / num2):.2f}")
+
+if _name_ == "_main_":
     main()
